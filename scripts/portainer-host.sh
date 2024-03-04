6a621a535ea4939cbaf031d278ac1a29b168816c
Markus Heiliger <markus.heiliger@microsoft.com>
2024-03-04 14:24:57 +0100

    Remove unnecessary image deletion commands

--- scripts/portainer-host.sh
+++ scripts/portainer-host.sh
@@ -19,11 +19,5 @@ docker rm -vf $(docker ps -q)
 echo ">>> Removing all containers ..."
 docker container prune -f
 
-echo ">>> Deleting all images ..."
-docker rmi -f $(docker images -q)
-
-echo ">>> Removing all images ..."
-docker image prune -f
-
 echo ">>> Installing Portainer ..."
 docker run --restart always -d -p 80:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
