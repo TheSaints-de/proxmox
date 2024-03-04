8299cb5918295ced3e1fe370bf7e6f3cc6798574
Markus Heiliger <markus.heiliger@microsoft.com>
2024-03-04 14:39:19 +0100

    Update Portainer container configuration

--- scripts/portainer-host.sh
+++ scripts/portainer-host.sh
@@ -25,8 +25,9 @@ docker volume create portainer_data
 echo ">>> Starting Portainer container..."
 docker run \
 	--restart always \
-	--detach \
-	--publish 80:9000 \
-	--volume /var/run/docker.sock:/var/run/docker.sock \
-	--volume portainer_data:/data \
+	-d \
+	-p 80:9000 \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v portainer_data:/data \
 	portainer/portainer-ce
+	
